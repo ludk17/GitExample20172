ccdc2e645e8583bb495eac51700711e1eecc37c0
cristianvt2 <kricas48@gmail.com>
2017-11-14 21:52:05 -0500

    new commit

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -10,7 +10,7 @@ namespace GitExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Cristian");
+            Console.WriteLine("Cristian Villoslada");
             Console.WriteLine("Luis Mendoza");
             Console.WriteLine("Kavs");
             Console.WriteLine("Jimmy Herrera");
