5810187b0af820143f6d84237c7d7fc989f89334
Luigui Mendoza <luigi.mendoza@bit2bitamericas.com>
2017-11-14 21:30:03 -0500

    nuevo texto

--- GitExample/Program.cs
+++ GitExample/Program.cs
@@ -11,6 +11,7 @@ namespace GitExample
         static void Main(string[] args)
         {
             Console.WriteLine("Luis Mendoza");
+            Console.WriteLine("Vamos Vamos Perú");
             Console.ReadLine();
         }
     }
